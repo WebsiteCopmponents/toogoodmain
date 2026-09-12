a062ce341e72bbebf148ea5b6418a4fb8fe6973c
luxhospitals-org <tools@luxhospitals.com>
2026-09-12 08:23:04 +0530

    Reorder components in Home page for improved layout: moved Industries and OurServices sections to follow About and precede FeaturesSection.

--- app/page.tsx
+++ app/page.tsx
@@ -18,9 +18,9 @@ export default function Home() {
     <>
       <Hero>
         <About />
-        <OurServices />
-        <Industries />
         <Services />
+        <Industries />
+        <OurServices />
         <FeaturesSection />
         <HowItWorks />
         <Pricing />
